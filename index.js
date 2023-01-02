7c340010791568c66e44b80c086f8d9598ae2df1
prabha1207 <98202075+prabha1207@users.noreply.github.com>
2023-01-02 15:27:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var x=1;
+let y=10;
+function showDateAndTime()
+{
+    document.getElementById("p").innerHTML=Date();
+    document.getElementById("h1").innerHTML="yes it is me";
+    document.getElementById("button").style.backgroundColor="red";
+    var x=10;
+    let y=20;
+    console.log("x="+x);
+console.log("y="+y);
+}
+console.log("x="+x);
+console.log("y="+y);
